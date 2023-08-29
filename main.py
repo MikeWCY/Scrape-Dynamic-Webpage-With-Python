070d0e1000467b07ecee9141a338989040527408
Mike WANG Chenyu <45159881+MikeWCY@users.noreply.github.com>
2023-08-29 10:57:08 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+url = ''
+
+## This part is for logging in the website
+username = ''
+password = ''
+
+## Initiate the webdriver
+chrome_driver_path = ''
+driver = webdriver.Chrome(service=ChromeService(chrome_driver_path))
+driver.get(url')
+driver.find_element(By.NAME, "username").send_keys(username)
+driver.find_element(By.NAME, "password").send_keys(password)
+           
+## Navigate and click the submit button
+driver.find_element(By.CSS_SELECTOR, "div[class='blue_button ']").click()
+
+
+## Get the webpage
+driver.get(url')    
